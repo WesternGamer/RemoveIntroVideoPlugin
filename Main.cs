5f355000496e991436ca04dcdb98b08e9c6e99ba
WesternGamer <80211714+WesternGamer@users.noreply.github.com>
2021-05-28 21:23:42 -0400

    Add project files.

---
+++ Main.cs
@@ -0,0 +1,37 @@
+﻿using Sandbox.Game;
+using VRage.Plugins;
+
+namespace RemoveIntroVideoPlugin
+{
+    /// <summary>
+    /// Where the plugin is started.
+    /// </summary>
+    public class Main : IPlugin
+    {
+        /// <summary>
+        /// The intro video is disabled here.
+        /// </summary>
+        public Main()
+        {
+            MyPlatformGameSettings.ENABLE_LOGOS = false;
+        }
+        /// <summary>
+        /// Unused
+        /// </summary>
+        public void Dispose()
+        {
+        }
+        /// <summary>
+        /// Unused
+        /// </summary>
+        public void Init(object gameInstance)
+        {
+        }
+        /// <summary>
+        /// Unused
+        /// </summary>
+        public void Update()
+        {           
+        }
+    }
+}
