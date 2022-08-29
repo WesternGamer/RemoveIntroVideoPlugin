554d6f01b8861f5d9bf79a824aef3c0d2d9a48bb
WesternGamer <Kevnguyen474@gmail.com>
2022-08-28 21:43:44 -0400

    Fixed certain main menu modifications not working with this plugin.

--- Main.cs
+++ Main.cs
@@ -1,5 +1,13 @@
 ﻿using Sandbox.Game;
+using Sandbox.Graphics;
+using Sandbox.Graphics.GUI;
+using SpaceEngineers.Game.GUI;
 using VRage.Plugins;
+using HarmonyLib;
+using System;
+using Sandbox.Game.Gui;
+using System.Threading;
+using VRageMath;
 
 namespace RemoveIntroVideoPlugin
 {
@@ -8,30 +16,64 @@ namespace RemoveIntroVideoPlugin
     /// </summary>
     public class Main : IPlugin
     {
+        private Type SEWorldGenMainMenu = AccessTools.TypeByName("SEWorldGenPlugin.GUI.MyPluginMainMenu");
+        private Type CustomLoadingBackgroundsMenu = AccessTools.TypeByName("CustomScreenBackgrounds.GUI.BackgroundScreen");
+        private bool MenuFixed = false;
+
         /// <summary>
-        /// The intro video is disabled here.
+        /// The intro video is disabled here. It is disabled in the constucter as it is called before the intro video plays. 
+        /// We cant disable the video in Init(object gameInstance) as it is called after the video started playing.
         /// </summary>
         public Main()
         {
             MyPlatformGameSettings.ENABLE_LOGOS = false;
         }
+
         /// <summary>
         /// Unused
         /// </summary>
         public void Dispose()
         {
+
         }
+
         /// <summary>
         /// Unused
         /// </summary>
         public void Init(object gameInstance)
         {
+            
         }
+
         /// <summary>
-        /// Unused
+        /// Fix for certain main menu modifications not working with this plugin.
         /// </summary>
         public void Update()
-        {           
+        {
+            if (MenuFixed)
+            {
+                return;
+            }
+
+            if (MyScreenManager.IsScreenOfTypeOpen(typeof(MyGuiScreenMainMenu)) && SEWorldGenMainMenu != null) //Checks if SEWorldGen is installed and if the main menu is open.
+            {
+                //Replaces the wrong main menu with the right one
+                MyScreenManager.CloseScreenNow(typeof(MyGuiScreenMainMenu));
+                MyScreenManager.AddScreen(MyGuiSandbox.CreateScreen(SEWorldGenMainMenu, false));
+                MenuFixed = true;
+                return;
+            }
+
+
+            if (MyScreenManager.IsScreenOfTypeOpen(typeof(MyGuiScreenMainMenu)) && CustomLoadingBackgroundsMenu != null)
+            {
+                MyScreenManager.CloseScreenNow(typeof(MyGuiScreenMainMenu));
+                MyScreenManager.AddScreen(MyGuiSandbox.CreateScreen(typeof(MyGuiScreenMainMenu), false));
+                MenuFixed = true;
+                return;
+            }
+
+            MenuFixed = true;
         }
     }
 }
---
+++ PostBuild.bat
@@ -0,0 +1,5 @@
+move "D:\VSProjects\RemoveIntroVideoPlugin\bin\Debug\RemoveIntroVideoPlugin.dll" "D:\Games\steamapps\common\SpaceEngineers\Bin64\Plugins\Local"
+C:
+cd "C:\Program Files (x86)\Steam"
+steam steam://rungameid/244850
+Exit
